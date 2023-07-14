1c1b18255f4c53e0d6c579276fd33330a6e1b7e9
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-15 01:19:57 +0800

    Change API to arrow function

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-async function getApi() {
+const getApi = async () => {
     const response = await fetch('https://api.weatherapi.com/v1/current.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
     const data = await response.json();
 
