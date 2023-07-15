660c9ef4b8ab126fc42c733cfeb8c35bd9a33203
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-15 14:39:02 +0800

    Remove getApiCurrent

--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,11 @@
 /*
-const getApiCurrent = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/current.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+const getApiForecast = async () => {
+    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
     const data = await response.json();
 
     console.log(data);
     return data;
 }
 
-const getApiForecast() = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: cors});
-    const data = await response.jason();
-
-    console.log(data);
-    return data;
-}
-
-getApiCurrent()
 getApiForecast()
 */
