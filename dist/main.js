eefd53eab2470361a8bb2b2f3fce24c58e558a14
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-15 17:23:40 +0800

    Add top right API functions

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,13 @@
-
 const getApiForecast = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+    const response = await fetch(urlApi, {mode: 'cors'});
     const data = await response.json();
 
-    console.log(data)
     return data;
 }
 
 //top left
 const getApiCondition = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+    const response = await fetch(urlApi, {mode: 'cors'});
     const data = await response.json();
 
     console.log(data.current.condition.text)
@@ -17,7 +15,7 @@ const getApiCondition = async () => {
 }
 
 const getApiLocation = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+    const response = await fetch(urlApi, {mode: 'cors'});
     const data = await response.json();
 
     console.log(data.location.name)
@@ -25,46 +23,54 @@ const getApiLocation = async () => {
 }
 
 //top right
+const feelsLike = document.getElementById('feelsLike');
 const getApiFeelsLikeC = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+    const response = await fetch(urlApi, {mode: 'cors'});
     const data = await response.json();
 
-    console.log(data.current.feelslike_c)
-    return data;
+    feelsLike.textContent = `${data.current.feelslike_c} °C`;
 }
 
 const getApiFeelsLikeF = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+    const response = await fetch(urlApi, {mode: 'cors'});
     const data = await response.json();
 
     console.log(data.current.feelslike_f)
     return data;
 }
 
+const humidity = document.getElementById('humidity');
 const getApiHumidity = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+    const response = await fetch(urlApi, {mode: 'cors'});
     const data = await response.json();
 
-    console.log(data.current.humidity)
+    humidity.textContent = `${data.current.humidity} %`;
     return data;
 }
 
+const rainChance = document.getElementById('rainChance');
 const getApiRainChance = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+    const response = await fetch(urlApi, {mode: 'cors'});
     const data = await response.json();
 
-    console.log(data.forecast.forecastday[0].day.daily_chance_of_rain)
-    return data;
+    rainChance.textContent = `${data.forecast.forecastday[0].day.daily_chance_of_rain} %`;
 }
 
+const windSpeed = document.getElementById('windSpeed');
 const getApiWindSpeed = async () => {
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
+    const response = await fetch(urlApi, {mode: 'cors'});
     const data = await response.json();
 
-    console.log(data.current.gust_kph
-        )
+    windSpeed.textContent = `${data.current.gust_kph} km/h`;
     return data;
 }
 
-getApiForecast()
+
+let urlApi = 'https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=singapore'
+
+getApiCondition();
+getApiFeelsLikeC();
+getApiHumidity();
+getApiRainChance();
+getApiWindSpeed();
 
