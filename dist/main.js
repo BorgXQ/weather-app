f7f85d2826702cd3602f4ea9cf298143d42e8c05
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-15 14:20:52 +0800

    Create new API for forecast

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,20 @@
 /*
-const getApi = async () => {
+const getApiCurrent = async () => {
     const response = await fetch('https://api.weatherapi.com/v1/current.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: 'cors'});
     const data = await response.json();
 
     console.log(data);
+    return data;
 }
 
-getApi()
+const getApiForecast() = async () => {
+    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=31ff6a003cc14b088ca164738231407&q=london', {mode: cors});
+    const data = await response.jason();
+
+    console.log(data);
+    return data;
+}
+
+getApiCurrent()
+getApiForecast()
 */
